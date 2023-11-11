4eded9718d6990e2132a868f44a07653d6f52ede
BhavyaMadaan11 <bhavyamadaan11@gmail.com>
2023-11-12 00:02:16 +0530

    card styles updated

--- src/components/services/Cards/Cards.jsx
+++ src/components/services/Cards/Cards.jsx
@@ -2,24 +2,25 @@ import React from 'react'
 import { useState } from "react"
 import "./Cards.css"
 
+
 export const Card = () => {
   const [cards] = useState([
     {
       title: "Courses",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar purus nunc, sit amet auctor sem aliquam et. Nunc vulputate varius magna in convallis. Vestibulum non sem interdum nibh eleifend pellentesque quis nec purus. Ut sit amet elit id nisl sollicitudin",
-      image: "../images/about.webp"
+      // image: "./components/services/Cards/about.jpg"
     },
   
     {
       title: "Projects",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar purus nunc, sit amet auctor sem aliquam et. Nunc vulputate varius magna in convallis. Vestibulum non sem interdum nibh eleifend pellentesque quis nec purus. Ut sit amet elit id nisl sollicitudin",
-      image: "../images/about.webp"
+      // image: ".components/services/Cards/about.jpg"
     },
     
     {
       title: "Clients",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar purus nunc, sit amet auctor sem aliquam et. Nunc vulputate varius magna in convallis. Vestibulum non sem interdum nibh eleifend pellentesque quis nec purus. Ut sit amet elit id nisl sollicitudin",
-      image: "../images/about.webp"
+      // image: "./components/services/Cards/about.jpg"
     },
   ]);
 
@@ -31,11 +32,11 @@ export const Card = () => {
           <div className='cards'>
             {
               cards.map((card,i)=>(
-                <div key={i} className='card'>
-                  <div className='card-heading'><h3>{card.title}</h3></div>
+                <div className='card'>
+                  <div className='card-heading'><h2>{card.title}</h2></div>
                   <div className='img-container'><img src={card.image} alt=''/></div>
                   <div className='card-text'><p>{card.description}</p></div>
-                  <div className='button'><button>Explore</button></div>
+                  <div className='explore-btn'><button>Explore</button></div>
                 </div>
             ))}
           </div>
