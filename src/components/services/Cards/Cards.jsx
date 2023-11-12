830dc954e54757302e3fd14f0bed51faf4d013ea
BhavyaMadaan11 <bhavyamadaan11@gmail.com>
2023-11-12 14:15:27 +0530

    card image updated

--- src/components/services/Cards/Cards.jsx
+++ src/components/services/Cards/Cards.jsx
@@ -8,19 +8,19 @@ export const Card = () => {
     {
       title: "Courses",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar purus nunc, sit amet auctor sem aliquam et. Nunc vulputate varius magna in convallis. Vestibulum non sem interdum nibh eleifend pellentesque quis nec purus. Ut sit amet elit id nisl sollicitudin",
-      // image: "./components/services/Cards/about.jpg"
+       image: "../images/about.jpg"
     },
   
     {
       title: "Projects",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar purus nunc, sit amet auctor sem aliquam et. Nunc vulputate varius magna in convallis. Vestibulum non sem interdum nibh eleifend pellentesque quis nec purus. Ut sit amet elit id nisl sollicitudin",
-      // image: ".components/services/Cards/about.jpg"
+       image: "../images/about.jpg"
     },
     
     {
       title: "Clients",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar purus nunc, sit amet auctor sem aliquam et. Nunc vulputate varius magna in convallis. Vestibulum non sem interdum nibh eleifend pellentesque quis nec purus. Ut sit amet elit id nisl sollicitudin",
-      // image: "./components/services/Cards/about.jpg"
+       image: "../images/about.jpg"
     },
   ]);
 
