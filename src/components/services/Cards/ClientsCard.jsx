1bc420b8aaae1801bbf4d64f3365295d63f1b81d
Aniketjha12 <aniketjha7784059626@gmail.com>
2023-11-14 00:12:07 +0530

    fast change

--- src/components/services/Cards/ClientsCard.jsx
+++ src/components/services/Cards/ClientsCard.jsx
@@ -3,10 +3,10 @@ import {Link} from "react-router-dom"
 export const ClientsCard = () => {
   return (
     <>
-    <section className="indianclients"></section>
+    <section className="clients"></section>
     <div>
         <h1>Our Indian Clients</h1>
-    <Link to="https://www.google.com/search?q=goo&rlz=1C1RXQR_enIN1082IN1082&oq=goo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTILCAEQRRgnGDsYigUyCQgCECMYJxiKBTIVCAMQLhhDGIMBGMcBGLEDGNEDGIoFMg8IBBAAGEMYgwEYsQMYigUyDAgFEAAYQxixAxiKBTIJCAYQABhDGIoFMgwIBxAAGEMYsQMYigUyDwgIEC4YQxivARjHARiKBTIMCAkQABhDGLEDGIoF0gEKMTM5ODVqMGoxNagCALACAA&sourceid=chrome&ie=UTF-8"><img src="https://www.accenture.com/in-en" alt="logo" /></Link>
+    <Link to="https://www.google.com/search?q=goo&rlz=1C1RXQR_enIN1082IN1082&oq=goo&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTILCAEQRRgnGDsYigUyCQgCECMYJxiKBTIVCAMQLhhDGIMBGMcBGLEDGNEDGIoFMg8IBBAAGEMYgwEYsQMYigUyDAgFEAAYQxixAxiKBTIJCAYQABhDGIoFMgwIBxAAGEMYsQMYigUyDwgIEC4YQxivARjHARiKBTIMCAkQABhDGLEDGIoF0gEKMTM5ODVqMGoxNagCALACAA&sourceid=chrome&ie=UTF-8"><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fimage&psig=AOvVaw17bpraXcRfedq8oKx0R7E0&ust=1699943932138000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPCA4-SuwIIDFQAAAAAdAAAAABAH" alt="logo" /></Link>
     </div>
     </>
   )
